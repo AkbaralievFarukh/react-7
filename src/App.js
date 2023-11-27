ddbea10a4b7760cd51e97058839a9ef7617a5a78
Farukh <akbfarukh@gmail.com>
2023-11-27 12:57:57 +0600

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import dayjs from 'dayjs';
+import React, { useEffect, useState } from 'react';
 
-function App() {
+const url = 'https://6564248aceac41c0761d7f0e.mockapi.io/todo'
+
+const App = () => {
+
+  const [todos, setTodos] = useState([])
+  const [todoTitle, setTodoTitle] = useState('')
+
+  useEffect(() => {
+    axios(url)
+      .then(({data}) => setTodos(data))
+  },[])
+
+  const handleAddTodo = () => {
+    const newTodo = {
+      title: todoTitle,
+      completed: false,
+      completedAt: null,
+      createdAt: +new Date(),
+    }
+    setTodoTitle('')
+    axios.post(url, newTodo)
+      .then(({data}) => setTodos([...todos, data]))
+  }
+
+  // const handleDeleteTodo = () => {
+  //   console.log(url);
+  // }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+        <h1>Todo list</h1>
+        <div>
+          <input type="text" 
+            onChange={(e) => setTodoTitle(e.target.value)}
+            value={todoTitle}
+          />
+          <button onClick={handleAddTodo}>Add</button>
+        </div>
+        {
+          todos.map(todo => 
+              <div key={todo.id} className='todo-wrapper'>
+                <p>{todo.title}</p>
+                <input type="checkbox" checked={todo.completed} />
+                <span>{dayjs(todo.data).format('DD:MM:YYYY HH:mm')}</span>
+              </div>
+            )
+        }
     </div>
   );
 }
